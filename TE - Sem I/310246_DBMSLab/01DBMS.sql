66181e75f84d3d821499b301d98f14d8a48185cf
Ayush Bulbule <ayushbulbule24@gmail.com>
2023-11-08 18:36:52 +0530

    Update 01DBMS.sql

--- TE - Sem I/310246_DBMSLab/01DBMS.sql
+++ TE - Sem I/310246_DBMSLab/01DBMS.sql
@@ -1,1 +1,84 @@
 -- ER Modeling and Normalization:
+
+---
+title: Entity-Relationship Modeling and Normalization
+date: 2023-11-08
+---
+
+# Entity-Relationship Modeling and Normalization
+
+## Introduction
+
+In the field of database design, Entity-Relationship (ER) modeling and normalization are crucial concepts. ER modeling helps in understanding the structure of the data, while normalization ensures data integrity and reduces redundancy. This document provides an overview of ER modeling and normalization.
+
+## Entity-Relationship Modeling
+
+### Entities
+
+Entities are the fundamental objects in a database. They represent real-world objects or concepts. Each entity is uniquely identified by a set of attributes and is typically represented as a rectangle in an ER diagram.
+
+Example Entity:
+```
+  Attributes: ID, First Name, Last Name, Email
+```
+
+### Relationships
+
+Relationships define how entities are related to each other. They describe how data is connected in the database. Relationships can be one-to-one, one-to-many, or many-to-many.
+
+Example Relationship:
+```
+  [Person] --(1:N)--> [Address]
+```
+
+
+## Normalization
+
+Normalization is the process of organizing data in a database to reduce redundancy and improve data integrity. The process involves dividing a table into smaller tables and defining relationships between them.
+
+### First Normal Form (1NF)
+
+To achieve 1NF, ensure that each column in a table contains only atomic values. No repeating groups or arrays should be present.
+
+Example 1NF Table:
+
+```
+[Orders]
+OrderID | Product1 | Product2 | Product3
+1 | Apple | Banana | Orange
+2 | Pear | Apple | NULL
+```
+
+### Second Normal Form (2NF)
+
+A table is in 2NF if it is in 1NF and all non-key attributes are fully functionally dependent on the primary key.
+
+Example 2NF Table:
+```
+  [OrderDetails]
+OrderID | ProductID | Quantity
+1 | 101 | 5
+1 | 102 | 3
+2 | 101 | 2
+```
+
+
+### Third Normal Form (3NF)
+
+A table is in 3NF if it is in 2NF and has no transitive dependencies. In other words, non-key attributes should not depend on other non-key attributes.
+
+Example 3NF Table:
+
+```
+[Customers]
+CustomerID | CustomerName | City
+101 | Alice | New York
+102 | Bob | Los Angeles
+```
+
+
+## Conclusion
+
+Entity-Relationship modeling and normalization are essential steps in creating well-structured and efficient databases. By properly designing entities and normalizing data, you can ensure data accuracy and maintainability in your database system.
+
+This document provides a brief overview of these concepts, but there is much more to explore in the world of database design.
